37d8085fd80cc0ce91c9576cb3853201d57b545f
Praveen Ravipati <ravipatipraveen77.45@gmail.com>
2014-05-23 11:20:40 +0530

    added my programs

---
+++ EightQueens/Equeen.java
@@ -0,0 +1,64 @@
+package com.eightqueen.test.programs;
+
+public class Equeen {
+        private static long startTime;
+	static int Matrix[] = new int[8];
+	static int sno = 0;
+        /*This method find the Solution N Queens(8 Queens)*/
+	public void EqueenFinder() {
+		System.out.println("the solutions are");
+		for (int row = 0; row >= 0;) {
+			while ((Matrix[row] < 8) && (!isSafeToPalce(row))) {
+				Matrix[row]++;
+			}
+			if (Matrix[row] > 7) {
+				Matrix[row] = 0;
+				row--;
+				if (row < 0) {
+				        System.out.println("\n\nTotal "+sno+" solutions\n");
+				        long endTime = System.currentTimeMillis();
+				        System.out.println("It took " + (endTime - startTime) + " milliseconds");
+				} else {
+					Matrix[row] = Matrix[row] + 1;
+				}
+
+			} else {
+				if (row == 7) {
+					out();
+					Matrix[row]++;
+				} else {
+					row++;
+					Matrix[row] = 0;
+				}
+			}
+		}
+
+	}
+        /*This Method checks whether at required position the element is insert or not*/
+	public boolean isSafeToPalce(int req_position) {
+		boolean safe = true;
+		for (int curr_row = 0; curr_row < req_position; curr_row++) {
+			if (Matrix[curr_row] == Matrix[req_position] || ((Math.abs(Matrix[curr_row] - Matrix[req_position])) == (Math.abs(curr_row - req_position)))) {
+				safe = false;
+				return safe;
+			}
+		}
+		return safe;
+	}
+        /*This method print the all solution*/
+	public void out() {
+		sno++;
+		System.out.print("\n " + sno + ": ");
+		for (int row = 0; row < 8; row++) {
+			System.out.print("(" + row + "," + Matrix[row] + ")");
+		}
+	}
+        /*main method*/
+	public static void main(String[] args) {
+                startTime = System.currentTimeMillis();
+		Equeen eq = new Equeen();
+		eq.EqueenFinder();
+	}
+
+}
+
---
+++ Threads/Bank_Transactions/using Integer/Method II/programs/BankTrans1.java
@@ -0,0 +1,50 @@
+package com.snapfish.threads.mainclasses;
+
+import java.io.File;
+import java.util.Hashtable;
+import java.util.Map.Entry;
+
+import com.snapfish.threads.Threads.*;
+import com.snapfish.threads.beans.AccountRecord;
+public class BankTrans1 {
+        /*main method*/
+	public static void main(String[] args) {
+		BankTrans1 st1=new BankTrans1();
+		Hashtable<Integer, AccountRecord> userAccount = new Hashtable<Integer, AccountRecord>();
+	        if (args.length != 1) {
+                        System.err.println("Invalid command line, exactly one argument required");
+                        System.exit(1);
+                }
+                String FilePath = args[0];
+		File folder = new File(FilePath);
+		File[] listOfFiles = folder.listFiles();
+
+		for (File file : listOfFiles) {
+			if (file.isFile()) {
+				MyThread t = new MyThread(file.getAbsolutePath(), userAccount);
+				t.start();
+				try {
+					t.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		st1.summary(userAccount);
+		//AccountRecord.displayAll();
+		if (userAccount != null) {
+			userAccount = null;
+		}
+		
+	}
+        /*this method prints the result*/
+	public void summary(Hashtable<Integer, AccountRecord> hashtable) {
+		System.out.println("\n\n\tSummary\n");
+		for (Entry<Integer, AccountRecord> entry : hashtable.entrySet()) {
+			AccountRecord Amount = entry.getValue();
+			System.out.println("The Account Id:\t" + Amount.getAccountId() + "\t\tAmount:\t"
+					+ Amount.getAmount());
+		}
+	}
+}
